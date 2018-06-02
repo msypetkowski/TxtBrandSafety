5b3687a2da21300e965e3ee571f94ee21a8d6d06
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-06-02 19:54:00 +0200

    Improve text extraction

--- html_to_texts.py
+++ html_to_texts.py
@@ -2,21 +2,36 @@
 
 import re
 import urllib.request
-    # import urllib2,cookielib
+from bs4 import BeautifulSoup
 
 from train_text_classifier import TextClassifier
 from word_model import WordModel
 
 
 def extract_texts_from_html(site):
-    txt = re.sub("(?s)<[^>]*>(\\s*<[^>]*>)*", r" ", site.decode()).replace('-->', '\n\n')
-    return txt.strip()
+    def additional_filter(site):
+        txt = re.sub("(?s)<[^>]*>(\\s*<[^>]*>)*", r" ", site.replace('-->', '\n'))
+        return txt.strip()
+    soup = BeautifulSoup(site, "html.parser")
+    data = soup.findAll(text=True)
+    def visible(element):
+        if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+            return False
+        elif re.match('<!--.*-->', str(element.encode('utf-8'))):
+            return False
+        return True
+    result = filter(visible, data)
+    result = [additional_filter(r) for r in result if r.strip()]
+    result = [r.strip() for r in result if r.strip()]
+    # print('\n'.join(result))
+    return list(result)
 
 
 def classify_website(site, text_classifier):
     site = extract_texts_from_html(site)
+    print('Website text blocks count:', len(site))
+    site = ' '.join(site)
     print('Website text length:', len(site))
-    # print(site)
     return text_classifier.classify_texts([site])[0]
 
 
@@ -43,6 +58,9 @@ def main():
     print(classify_website(html, text_classifier))
 
     html = fetch_url('https://www.tripadvisor.com/Restaurant_Review-g29220-d5768632-Reviews-Hali_imaile_General_Store-Maui_Hawaii.html')
+    print(classify_website(html, text_classifier))
+
+    html = fetch_url('https://www.overstock.com/Electronics/2/store.html')
     print(classify_website(html, text_classifier))
 
 if __name__ == '__main__':
